0c061a4ba9c7303a835e9cc9ec9b3a06dee5677d
gsmolinski <g.smolinski1@gmail.com>
2022-01-12 18:03:22 +0100

    keydown listener added

--- R/shinybrowser_mod.R
+++ R/shinybrowser_mod.R
@@ -1,18 +1,31 @@
+#' UI module
+#'
+#' Adds keydown listener.
+#'
+#' @param id arg from shinybrowserServer. Can be chosen by user.
+#'
+#' @return JavaScript code which adds event listener
+#' @import shiny
 shinybrowserUI <- function(id) {
   ns <- NS(id)
-  tagList(
-
+  inputId <- ns("key_pressed")
+  js <- glue::glue(
+    'document.addEventListener("keydown", function(e) {{
+     Shiny.onInputChange({inputId}, e.key);
+    }})'
   )
+  tags$script(js)
 }
 
 shinybrowserServer <- function(enabled = TRUE,
                                id = "shinybrowser",
+                               keyEvent = "F1",
                                session = getDefaultReactiveDomain()) {
 
-  check_requirements_shinybrowserServer(enabled, id, session)
+  check_requirements_shinybrowserServer(enabled, id, keyEvent, session)
 
   if (enabled) {
-
+    insertUI("head", "beforeEnd", shinybrowserUI(id), immediate = TRUE)
     moduleServer(
       id,
       function(input, output, session) {
--- R/utils.R
+++ R/utils.R
@@ -1,14 +1,22 @@
-check_requirements_shinybrowserServer <- function(enabled, id, session) {
+check_requirements_shinybrowserServer <- function(enabled, id, keyEvent, session) {
 
   if (!is.logical(enabled) || is.na(enabled) || length(enabled) > 1) {
-    stop("'enabled' must be TRUE or FALSE.", call. = FALSE)
+    stop("'enabled' must be TRUE or FALSE.",
+         call. = FALSE)
   }
 
   if (!is.character(id) || is.na(id) || length(id) > 1 || id == "") {
-    stop("'id' must be of type character of length 1 and can't be NA or empty.", call. = FALSE)
+    stop("'id' must be of type character of length 1 and can't be NA or empty.",
+         call. = FALSE)
+  }
+
+  if (!is.character(keyEvent) || is.na(keyEvent) || length(keyEvent) > 1 || keyEvent == "") {
+    stop("'keyEvent' must be of type character of length 1 and can't be NA or empty.",
+         call. = FALSE)
   }
 
   if (is.null(session)) {
-    stop("No Shiny session object was found.", call. = FALSE)
+    stop("No Shiny session object was found.",
+         call. = FALSE)
   }
 }

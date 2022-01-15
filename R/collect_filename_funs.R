1d4b0f595916fc0ae5c37c5be8609e9e8b22edaa
gsmolinski <g.smolinski1@gmail.com>
2022-01-15 21:01:44 +0100

    now keeps only unnested functions

--- R/collect_filename_funs.R
+++ R/collect_filename_funs.R
@@ -1,35 +1,41 @@
-#' Get Full Paths to Filenames, Filenames and Functions from Chosen Environments
+#' Get Full Paths to Filenames, Filenames and Functions from Chosen Environments and
+#' Omit Nested Functions
 #'
 #' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
 #' function used directly by user.
 #'
-#' @return data.frame with cols: filename_full_path, filename, fun_name
+#' @return data.frame with cols: filename_full_path, filename, fun_name.
+#' @details
+#' It keps only these functions from each source file, which are top functions, i.e. it omits
+#' nested functions. It is because we want to use 'body()' later, so we will have access
+#' to these nested functions.
 #' @importFrom magrittr %>%
 #' @noRd
 collect_filename_funs <- function(caller_env) {
   envs_funs <- lapply(rlang::env_parents(caller_env), names)
   envs_funs <- drop_envs_too_far(envs_funs)
-  envs_funs <- unlist(envs_funs, use.names = FALSE)
+  names(envs_funs) <- paste0(1:length(envs_funs), "_")
+  envs_funs <- unlist(envs_funs, use.names = TRUE)
+  names(envs_funs) <- gsub("_\\d*", replacement = "", x = names(envs_funs)) # env depth level
 
-  filename_srccode <- data.frame(filename_full_path = character(),
-                                 filename = character(),
-                                 fun_name = character())
-  filename_srccode <- filename_srccode %>%
+  filenames_funs <- data.frame()
+  filenames_funs <- filenames_funs %>%
     dplyr::mutate(filename_full_path = vapply(envs_funs, get_filename, FUN.VALUE = character(1)),
                   filename = basename(filename_full_path),
-                  fun_name = envs_funs)
+                  fun_name = envs_funs,
+                  env_depth = as.integer(names(envs_funs))) %>%
+    dplyr::filter(!is.na(filename_full_path)) %>%
+    dplyr::group_by(filename_full_path) %>%
+    dplyr::slice_max(env_depth) %>% # keep only unnested functions
+    dplyr::ungroup() %>%
+    dplyr::select(-env_depth)
 
-  # keep only top-of-the-top funs from each file which have srcref
-  filename_srccode <- as.data.frame(lapply(filename_srccode, rev))
-  filename_srccode <- filename_srccode %>%
-    dplyr::filter(!duplicated(filename_full_path) & !is.na(filename_full_path))
-
-  filename_srccode
+  filenames_funs
 }
 
-#' Remove Not Needed Environemnts
+#' Remove Not Needed Environments
 #'
-#' @param envs_funs named list with functions from found environments
+#' @param envs_funs named list with functions from found environments.
 #'
 #' @details
 #' If list has at least one name 'namespace:' it means app is developed as package.
@@ -41,7 +47,7 @@ collect_filename_funs <- function(caller_env) {
 #' as a package. Then we just need to remove global environment, because there won't be
 #' reactive functions.
 #'
-#' @return list without global environment and without environments from other packages
+#' @return list without global environment and without environments from other packages.
 #' @noRd
 drop_envs_too_far <- function(envs_funs) {
   if (any(grepl("namespace:", names(envs_funs)))) {
@@ -55,10 +61,10 @@ drop_envs_too_far <- function(envs_funs) {
 
 #' Get Filename with Full Path for Function
 #'
-#' @param one_envs_funs one element of envs_funs
+#' @param one_envs_funs one element of envs_funs.
 #'
 #' @return full path for function if functon was sourced from file,
-#' otherwise NA_character_
+#' otherwise NA_character_.
 #' @noRd
 get_filename <- function(one_envs_funs) {
   filename <- attr(attr(get(one_envs_funs),"srcref"),"srcfile")$filename

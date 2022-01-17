694360e2cfb3b17b594607c8351c4f3a52808f75
gsmolinski <g.smolinski1@gmail.com>
2022-01-17 09:35:24 +0100

    Add data returned by getParseData

--- R/collect_filenames_funs_parseData.R
+++ R/collect_filenames_funs_parseData.R
@@ -1,17 +1,16 @@
-#' Get Full Paths to Filenames, Filenames and Functions from Chosen Environments and
-#' Omit Nested Functions
+#' Get Filenames, Functions Names and Parse Data
 #'
 #' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
 #' function used directly by user.
 #'
-#' @return data.frame with cols: filename_full_path, filename, fun_name.
+#' @return data.frame with cols: filename_full_path, filename, fun_name, parse_data.
 #' @details
 #' It keps only these functions from each source file, which are top functions, i.e. it omits
 #' nested functions. It is because we want to use 'body()' later, so we will have access
 #' to these nested functions.
 #' @importFrom magrittr %>%
 #' @noRd
-collect_filename_funs <- function(caller_env) {
+collect_filenames_funs_parseData <- function(caller_env) {
   envs_funs <- lapply(rlang::env_parents(caller_env), names)
   envs_funs <- drop_envs_too_far(envs_funs)
   names(envs_funs) <- paste0(1:length(envs_funs), "_")
@@ -30,7 +29,8 @@ collect_filename_funs <- function(caller_env) {
     dplyr::group_by(filename_full_path) %>%
     dplyr::slice_max(env_depth) %>% # keep only unnested functions
     dplyr::ungroup() %>%
-    dplyr::select(-env_depth)
+    dplyr::select(-env_depth) %>%
+    dplyr::mutate(parse_data = lapply(envs_funs, get_parse_data, env_caller = env_caller))
 
   filenames_funs
 }
@@ -41,7 +41,7 @@ collect_filename_funs <- function(caller_env) {
 #'
 #' @details
 #' If list has at least one name 'namespace:' it means app is developed as package.
-#' Then 'env_parents()' from 'collect_filename_funs' function will return other
+#' Then 'env_parents()' from 'collect_filenames_funs_parseData' function will return other
 #' packages as well and after them global environment. We don't need them, because
 #' there won't be, developed by user, reactive functions.
 #'
@@ -64,6 +64,8 @@ drop_envs_too_far <- function(envs_funs) {
 #' Get Filename with Full Path for Function
 #'
 #' @param one_envs_funs one element of envs_funs.
+#' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
+#' function used directly by user.
 #'
 #' @return full path for function if functon was sourced from file,
 #' otherwise NA_character_.
@@ -74,4 +76,17 @@ get_filename <- function(one_envs_funs, caller_env) {
     filename <- NA_character_
   }
   filename
+}
+
+#' Use 'getParseData' on Object
+#'
+#' @param one_envs_funs one element of envs_funs.
+#' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
+#' function used directly by user.
+#'
+#' @return data.frame returned by 'getParseData()'.
+#' @noRd
+get_parse_data <- function(one_envs_funs, caller_env) {
+  parse_data <- getParseData(get(one_envs_funs, envir = caller_env), includeText = NA)
+  parse_data
 }

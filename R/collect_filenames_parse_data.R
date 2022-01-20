1b9af876c99c968ec2b0c5b8e810e2c699b11a32
gsmolinski <g.smolinski1@gmail.com>
2022-01-20 11:18:52 +0100

    Now to 'get' is passed not caller env, but the env where object lives

--- R/collect_filenames_parse_data.R
+++ R/collect_filenames_parse_data.R
@@ -1,4 +1,6 @@
-#' Get Filenames and Parse Data
+#' Colect Filenames and Parse Data
+#'
+#' It collects results from 'get_filename_parse_data' function.
 #'
 #' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
 #' function used directly by user.
@@ -7,47 +9,26 @@
 #' @importFrom magrittr %>%
 #' @noRd
 collect_filenames_parse_data <- function(caller_env) {
-  envs_objs <- find_objects_in_envirs(caller_env)
-  envs_objs <- drop_envs_too_far(envs_objs)
-  envs_objs <- unlist(envs_objs, use.names = FALSE)
+  envirs <- rlang::env_parents(caller_env)
+  envirs <- drop_envs_too_far(envirs)
+  filenames_parse_data <- lapply(envirs, get_filenames_parse_data)
+  filenames_parse_data <- dplyr::bind_rows(filenames_parse_data)
 
-  filenames_parse_data <- data.frame(
-    filename_full_path = vapply(envs_objs, get_filename, FUN.VALUE = character(1),
-                                caller_env = caller_env),
-    obj_name = envs_objs
-  )
   filenames_parse_data <- filenames_parse_data %>%
-    dplyr::filter(!is.na(filename_full_path) & !duplicated(filename_full_path)) %>%
-    dplyr::mutate(filename = basename(filename_full_path),
-                  parse_data = lapply(obj_name, get_parse_data, caller_env = caller_env)) %>%
-    dplyr::select(-obj_name)
+    dplyr::filter(!duplicated(filename_full_path)) %>%
+    dplyr::mutate(filename = basename(filename_full_path)) %>%
+    dplyr::relocate(filename, .before = dplyr::last_col())
 
   filenames_parse_data
 }
 
-#' Get Names of Objects from Current and Parent Environments
-#'
-#' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
-#' function used directly by user.
-#'
-#' @return named list if environment has name with names (character type) of objects belong
-#' to each environment.
-#' @details
-#' Function starts from current environment, which is a caller environment of exported function,
-#' i.e. 'shinybrowserServer' and go through the parent environments, i.e. environments returned
-#' by 'rlang::env_parents()' function.
-#' @noRd
-find_objects_in_envirs <- function(caller_env) {
-  lapply(rlang::env_parents(caller_env), names)
-}
-
 #' Remove Not Needed Environments
 #'
-#' @param envs_objs named list with objects from found environments.
+#' @param envs_objs named list with found environments.
 #'
 #' @details
 #' If list has at least one name 'namespace:' it means app is developed as package.
-#' Then 'env_parents()' from 'find_objects_in_envirs' function will return other
+#' Then 'env_parents()' function will return other
 #' packages as well and after them global environment. We don't need them, because
 #' there won't be, developed by user, reactive functions.
 #'
@@ -57,27 +38,54 @@ find_objects_in_envirs <- function(caller_env) {
 #'
 #' @return list without global environment and without environments from other packages.
 #' @noRd
-drop_envs_too_far <- function(envs_objs) {
-  if (any(grepl("namespace:", names(envs_objs)))) {
-    first_namespace <- grep("namespace:", names(envs_objs))[[1]]
-    envs_objs <- envs_objs[1:first_namespace]
+drop_envs_too_far <- function(envirs) {
+  if (any(grepl("namespace:", names(envirs)))) {
+    first_namespace <- grep("namespace:", names(envirs))[[1]]
+    envirs <- envirs[1:first_namespace]
   } else {
-    envs_objs[["global"]] <- NULL
+    envirs[["global"]] <- NULL
   }
-  envs_objs
+  envirs
+}
+
+#' Get Full Path to File and Parse Data for Object
+#'
+#' @param envir each environment returned by 'rlang::env_parents()',
+#' passed by 'collect_filenames_parse_data' function.
+#'
+#' @return data.frame with cols: filename_full_path, parse_data
+#' @noRd
+get_filenames_parse_data <- function(envir) {
+  env_objs <- names(envir)
+
+  if (length(env_objs) > 0) {
+    filenames_parse_data <- data.frame(
+      filename_full_path = vapply(env_objs, get_filename, FUN.VALUE = character(1),
+                                  envir = envir),
+      obj_name = env_objs
+    )
+
+    filenames_parse_data <- filenames_parse_data %>%
+      dplyr::filter(!is.na(filename_full_path) & !duplicated(filename_full_path)) %>%
+      dplyr::mutate(parse_data = lapply(obj_name, get_parse_data, envir = envir)) %>%
+      dplyr::select(-obj_name)
+
+    filenames_parse_data
+  } else {
+    NULL
+  }
 }
 
 #' Get Filename with Full Path for Object
 #'
 #' @param one_envs_objs one element of envs_objs
-#' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
-#' function used directly by user.
+#' @param envir environment where object lives, passed to 'get'.
 #'
 #' @return full path for object if object was sourced from file,
 #' otherwise NA_character_.
 #' @noRd
-get_filename <- function(one_envs_objs, caller_env) {
-  filename <- attr(attr(get(one_envs_objs, envir = caller_env),"srcref"),"srcfile")$filename
+get_filename <- function(one_envs_objs, envir) {
+  filename <- attr(attr(get(one_envs_objs, envir = envir),"srcref"),"srcfile")$filename
   if (is.null(filename)) {
     filename <- NA_character_
   }
@@ -87,12 +95,11 @@ get_filename <- function(one_envs_objs, caller_env) {
 #' Use 'getParseData' on Object
 #'
 #' @param one_obj_name one element of obj_name column.
-#' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
-#' function used directly by user.
+#' @param envir environment where object lives, passed to 'get'.
 #'
 #' @return data.frame returned by 'getParseData()'.
 #' @noRd
-get_parse_data <- function(one_obj_name, caller_env) {
-  parse_data <- getParseData(get(one_obj_name, envir = caller_env), includeText = NA)
+get_parse_data <- function(one_obj_name, envir) {
+  parse_data <- getParseData(get(one_obj_name, envir = envir), includeText = NA)
   parse_data
 }

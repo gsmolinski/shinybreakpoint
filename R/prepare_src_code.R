4770057b93572912f79cf586d689d8a4bb569e13
gsmolinski <g.smolinski1@gmail.com>
2022-01-26 12:03:41 +0100

    add documentation for each function

--- R/prepare_src_code.R
+++ R/prepare_src_code.R
@@ -1,3 +1,15 @@
+#' Return Prepared Source Code and Environments
+#'
+#' @param caller_env object returned by 'rlang::caller_env()', passed by exported function, i.e.
+#' function used directly by user.
+#'
+#' @return
+#' Named list:
+#' 'filenames_parse_data': data.frame with full path to file, file name and parse data,
+#' but only if file contained at least one reactive funtion and this funtion wasn't live in
+#' Global Environment (because we have removed Global Environment previously from search path).
+#' 'envirs': named list of environments for each file.
+#' @noRd
 prepare_src_code <- function(caller_env) {
   filenames_parse_data_env <- collect_filenames_parse_data(caller_env)
   if (!is.null(filenames_parse_data)) {
@@ -17,15 +29,43 @@ prepare_src_code <- function(caller_env) {
   }
 }
 
+#' Find and Left Reactives
+#'
+#' Removes not reactives functions from parse_data.
+#'
+#' @param parse_data one data.frame with parse data.
+#'
+#' @return
+#' Named list:
+#' 'parse_data': data.frame for each path (file) with exprs (top id) contain reactive fun
+#' or NULL if no exprs contain reactive fun.
+#' 'which_null': indices where are NULLs in returning list.
+#' @noRd
 find_left_reactives <- function(parse_data) {
-  parse_data <- lapply(parse_data, find_direct_parent_id)
+  parse_data <- lapply(parse_data, find_direct_parent_id_with_reactive)
   parse_data <- lapply(parse_data, remove_nested_reactives)
   which_null <- which(vapply(parse_data, is.null, FUN.VALUE = logical(1)))
   list(parse_data = parse_data,
        which_null = which_null)
 }
 
-find_direct_parent_id <- function(one_parse_data) {
+#' Find Expressions Contain Reactives
+#'
+#' @param one_parse_data one data.frame with parse data.
+#'
+#' @return
+#' data.frame with parse data, but only with exprs contain reactives
+#' if at least one reactive was found, otherwise NULL.
+#' @details
+#' utils::getParseData with argument 'NA' passed to parameter 'includeText'
+#' does not return text for 'expr' token, but we will use later function utils::getParseText
+#' to retrieve these texts. However, it is needed to find top id (for expr), so we are sure
+#' we have whole reactive function. Also, 'find_direct_parent_id_with_reactive' returns
+#' only exprs which are reactives. To find reactives, regex is used - this regex may need to be
+#' updated in next 'Shiny' versions and should be taken into account that other packages
+#' can export their reactives and should be find by this function as well.
+#' @noRd
+find_direct_parent_id_with_reactive <- function(one_parse_data) {
   extracted_lines <- one_parse_data$line1[grep("^reactive$|^eventReactive$|^observe$|^observeEvent$|^render[A-Z]+",
                                              one_parse_data$text, perl = TRUE)]
   if (length(extracted_lines) > 0) {
@@ -36,6 +76,13 @@ find_direct_parent_id <- function(one_parse_data) {
   }
 }
 
+#' Remove Nested Reactives
+#'
+#' @param one_parse_data one data.frame with parse data or NULL if no parse data.
+#'
+#' @return
+#' Parse data (data.frame) without exprs which were nested or NULL if NULL was passed into.
+#' @noRd
 remove_nested_reactives <- function(one_parse_data) {
   if (!is.null(one_parse_data)) {
     shifted_line2 <- dplyr::lag(one_parse_data$line2, n = 1)
@@ -50,6 +97,15 @@ remove_nested_reactives <- function(one_parse_data) {
   }
 }
 
+#' Check if Reactive Is Nested
+#'
+#' @param indice one indice of column 'line2' from parse data.
+#' @param line2 whole column 'line2' from parse data (last line of each expression).
+#' @param shifted_line2 whole column 'line2' from parse data, but modified by dplyr::lag.
+#'
+#' @return
+#' TRUE if reactive is nested in any other reactive, otherwise FALSE.
+#' @noRd
 is_nested_reactive <- function(indice, line2, shifted_line2) {
   line2 <- line2[[indice]]
   shifted_line2 <- shifted_line2[seq_len(indice)]
@@ -66,6 +122,21 @@ is_nested_reactive <- function(indice, line2, shifted_line2) {
   }
 }
 
+#' Get Text for Expr and Format It
+#'
+#' Source code is retrieved using utils::getParseText and then splitted to separate lines.
+#' Line indicators are added as well.
+#'
+#' @param one_parse_data one data.frame with parse data.
+#'
+#' @return
+#' data.frame with col 'line' where is line number for source code
+#' and col 'src_code' with formatted source code.
+#' @details
+#' Each block of code is separated by empty row (NA in both columns). This is only
+#' for readability and empty row is added also if is not present in an original
+#' source code, i.e. in file.
+#' @noRd
 retrieve_src_code <- function(one_parse_data) {
   lines <- seq_vec(one_parse_data$line1, one_parse_data$line2)
   lines[-length(lines)] <- lapply(lines[-length(lines)], append, values = NA_integer_)

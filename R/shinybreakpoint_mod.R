624eab8a53f17215ba24e703132906bd4b6bbad1
gsmolinski <g.smolinski1@gmail.com>
2022-02-23 13:59:35 +0100

    add counter to js script

--- R/shinybreakpoint_mod.R
+++ R/shinybreakpoint_mod.R
@@ -4,16 +4,24 @@
 #'
 #' @param id from 'shinybreakpointServer'. Can be chosen by user.
 #'
-#' @return HTML script tag with JavaScript code.
+#' @return HTML script tag with JavaScript code - returns
+#' key pressed and counter (+1 after any key pressed).
+#' @details
+#' It is needed to add counter, because otherwise when
+#' the same key will be pressed in a row, the second
+#' input wouldn't have any effect.
 #' @import shiny
 shinybreakpointUI <- function(id) {
   ns <- NS(id)
-  inputId <- ns("key_pressed")
-  js <- glue::glue_safe(
-    'document.addEventListener("keydown", function(e) {{
-     Shiny.onInputChange({inputId}, e.key);
-    }})'
-  )
+  key_counted <- ns("key_counted")
+  key_pressed <- ns("key_pressed")
+  js <- glue::glue_safe('
+    let counter = 0;
+    document.addEventListener("keydown", function(e) {{
+     Shiny.onInputChange("{key_counted}", counter++);
+     Shiny.onInputChange("{key_pressed}", e.key);
+    }})
+  ')
   tags$script(js)
 }
 
--- tests/testthat/test-shinybreakpoint_mod.R
+++ tests/testthat/test-shinybreakpoint_mod.R
@@ -2,8 +2,10 @@ test_that("js code in shinybreakpointUI is correct", {
   val <- shinybreakpointUI("test")
   expect_equal(
   val$children[[1]],
-  'document.addEventListener("keydown", function(e) {
- Shiny.onInputChange(test-key_pressed, e.key);
-})'
+  '  let counter = 0;
+  document.addEventListener("keydown", function(e) {
+   Shiny.onInputChange("test-key_counted", counter++);
+   Shiny.onInputChange("test-key_pressed", e.key);
+  })'
 ) # assumes shiny::NS(id) behavior is to add '{id}-' before inputId
 })

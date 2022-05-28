66d11e438c2ee612bdc85a7d07f8686f6b998e95
gsmolinski <g.smolinski1@gmail.com>
2022-05-28 12:55:46 +0200

    use replaceAll to target all occurences

--- inst/www/js/colorize_code.js
+++ inst/www/js/colorize_code.js
@@ -11,14 +11,14 @@ function (cellInfo) {
         code = code.replace(/\b((?:TRUE|FALSE|T|F|NA|NA_character_|NA_integer_|NA_complex_|NA_real_|NULL))\b/g, "<span class = 'specials_code'>$1</span>");
         code = code.replace(/\b((?:if|else|repeat|while|for|in|next|break))\b/g, "<span class = 'keyword_code'>$1</span>");
         code = code.replace(/\b([-+]?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?i?|Inf|NaN))\b/g, "<span class = 'number_code'>$1</span>");
-        code = code.replace("#", "<span class = 'comment_code'>#</span>");
-        code = code.replace("(", "<span class = 'bracket_code'>(</span>");
-        code = code.replace(")", "<span class = 'bracket_code'>)</span>");
-        code = code.replace("{", "<span class = 'brace_code'>{</span>");
-        code = code.replace("}", "<span class = 'brace_code'>}</span>");
-        code = code.replace("[", "<span class = 'select_code'>[</span>");
-        code = code.replace("]", "<span class = 'select_code'>]</span>");
-        code = code.replace("$", "<span class = 'select_code'>$</span>");
+        code = code.replaceAll("#", "<span class = 'comment_code'>#</span>");
+        code = code.replaceAll("(", "<span class = 'bracket_code'>(</span>");
+        code = code.replaceAll(")", "<span class = 'bracket_code'>)</span>");
+        code = code.replaceAll("{", "<span class = 'brace_code'>{</span>");
+        code = code.replaceAll("}", "<span class = 'brace_code'>}</span>");
+        code = code.replaceAll("[", "<span class = 'select_code'>[</span>");
+        code = code.replaceAll("]", "<span class = 'select_code'>]</span>");
+        code = code.replaceAll("$", "<span class = 'select_code'>$</span>");
         return React.createElement("span", {
             dangerouslySetInnerHTML: { __html: code }
         });

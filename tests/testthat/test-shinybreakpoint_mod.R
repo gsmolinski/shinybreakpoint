566b87dc698e4e24bea27a49413737c1d0ab3d35
gsmolinski <g.smolinski1@gmail.com>
2022-09-11 13:44:10 +0200

    tests to test regular functions mostly in shinybreakpoint_mod added

--- tests/testthat/test-shinybreakpoint_mod.R
+++ tests/testthat/test-shinybreakpoint_mod.R
@@ -4,6 +4,9 @@ e <- new.env(parent = .GlobalEnv)
 e$server_test <- server
 g <- new.env(parent = e)
 
+reactlog_data_path <- system.file("tests_helpers", "reactlog_data.rds", package = "shinybreakpoint")
+reactlog_data <- readRDS(reactlog_data_path)
+
 test_that("js code in shinybreakpointUI is correct", {
   val <- shinybreakpointUI("test")
   expect_equal(
@@ -26,6 +29,48 @@ test_that("'validate_id' returns error if reactlog_data is NULL or labels
                          regexp = "These Ids or labels are duplicated: label1, label3. Use only unique Ids and labels.")
           })
 
+test_that("'create_UI' displays simple info with no source code
+          if no source code found with reactive objects", {
+            expected <- tags$div(class = "no-file",
+                                 tags$div(class = "circle-div",
+                                          tags$div(class = "circle")),
+                                 tags$p("There is nothing to see here"))
+            expect_identical(create_UI(NULL, NULL, NULL),
+                             expected)
+            df_no_row <- data.frame(a = 1,
+                                    b = 2)
+            df_no_row <- df_no_row[FALSE, ]
+            expect_identical(create_UI(NULL, df_no_row, NULL),
+                             expected)
+          })
+
+test_that("'get_dependencies_set_names' returns named list", {
+  id <- "text_output"
+  filenames_parse_data <- list(
+    filename = c("app.R", "app.R", "mod1.R"),
+    filename_full_path = c("a/app.R", "b/app.R", "c/mod1.R"),
+    parse_data = list(
+      data.frame(line = c(11, 12, 13, NA, 17, 18, NA),
+                 src_code = c("a", "b", "c", NA, "d", "e", NA)),
+      data.frame(line = c(20, 21, 22, NA, 24, 25, NA),
+                 src_code = c("aa", "bb", "cc", NA, "dd", "ee", NA)),
+      data.frame(line = c(1, NA, 2, NA),
+                 src_code = c("aaa", NA, "bbb", NA))
+    )
+  )
+  binded_filenames_parse_data <- prepare_filenames_parse_data(filenames_parse_data)
+  labelled_reactive_objects <- data.frame(location_object = c(1, 2),
+                                          label = c("observe event with text", "observe with input text"),
+                                          file = c("mod1.R", "mod1.R"))
+  ids_dependency_df <- prepare_dependency_df_and_ids_data(reactlog_data, labelled_reactive_objects)
+  reactlog_dependency_df <- ids_dependency_df$reactlog_dependency_df
+  ids_data <- ids_dependency_df$ids_data
+  result <- get_dependencies_set_names(id, find_dependencies, binded_filenames_parse_data, reactlog_dependency_df, ids_data)
+
+  expect_type(result, "list")
+  expect_named(result, "text_output")
+})
+
 server <- function(id) {
   shinybreakpointServer(id = "shinybreakpoint")
 }
@@ -39,65 +84,3 @@ test_that("'get_files' returns named list", {
 })
 
 
-
-# test_that("'which_file' reactive returns indice where is file", {
-#
-#   skip_on_ci()
-#
-#   testServer(server, {
-#     filenames_src_code_envirs <- prepare_src_code(g)
-#     session$setInputs(file = filenames_src_code_envirs$filenames_parse_data$filename_full_path)
-#     expect_equal(which_file(), 1)
-#   })
-# })
-#
-# test_that("'selected_line' reactive returns not truthy object if no line selected
-#           or NA (empty line) selected and truthy otherwise", {
-#
-#   skip_on_ci()
-#
-#   testServer(server, {
-#     filenames_src_code_envirs <- prepare_src_code(g)
-#     session$setInputs(file = filenames_src_code_envirs$filenames_parse_data$filename_full_path,
-#                       src_code__reactable__selected = NULL)
-#     expect_false(isTruthy(selected_line()))
-#     session$setInputs(src_code__reactable__selected = 22)
-#     expect_false(isTruthy(selected_line()))
-#     session$setInputs(src_code__reactable__selected = 14)
-#     expect_true(isTruthy(selected_line()))
-#   })
-# })
-# # don't know why it has to be repeated, but otherwise don't work
-# server <- function(id) {
-#   shinybreakpointServer(id = "shinybreakpoint")
-# }
-#
-# test_that("'object' reactive returns error (do not run) if NA (empty line) selected
-#           and truthy otherwise", {
-#   skip_if_not(interactive())
-#   testServer(server, {
-#     filenames_src_code_envirs <- prepare_src_code(g)
-#     session$setInputs(file = filenames_src_code_envirs$filenames_parse_data$filename_full_path,
-#                       src_code__reactable__selected = 22)
-#     expect_error(req(object()))
-#     session$setInputs(src_code__reactable__selected = 14)
-#     expect_true(isTruthy(object()))
-#   })
-# })
-# # don't know why it has to be repeated, but otherwise don't work
-# server <- function(id) {
-#   shinybreakpointServer(id = "shinybreakpoint")
-# }
-#
-# test_that("'breakpoint_can_be_set' reactive returns FALSE (not truthy) if not possible
-#           to put browser and TRUE (truthy) otherwise", {
-#   skip_if_not(interactive())
-#   testServer(server, {
-#     filenames_src_code_envirs <- prepare_src_code(g)
-#     session$setInputs(file = filenames_src_code_envirs$filenames_parse_data$filename_full_path,
-#                       src_code__reactable__selected = 23)
-#     expect_false(isTruthy(breakpoint_can_be_set()))
-#     session$setInputs(src_code__reactable__selected = 14)
-#     expect_true(isTruthy(breakpoint_can_be_set()))
-#   })
-# })

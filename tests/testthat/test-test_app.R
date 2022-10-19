0b9cea91f5421c2163e692d5202befecdb2b49be
gsmolinski <g.smolinski1@gmail.com>
2022-10-19 09:21:28 +0200

    skip shinytest2 on ci

--- tests/testthat/test-test_app.R
+++ tests/testthat/test-test_app.R
@@ -2,6 +2,7 @@
 library(shinytest2)
 
 test_that("all tests for test_app pass", {
+  skip_on_ci()
   app_dir <- file.path(system.file(package = "shinybreakpoint", "tests_helpers"),
                        "test_app")
   test_app(app_dir)
